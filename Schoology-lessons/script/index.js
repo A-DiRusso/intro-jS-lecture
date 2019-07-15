d2051c48e066aeab19252869b23dca7a2f4c978a
Anthony DiRusso <ossurid@gmail.com>
2019-07-15 12:46:43 -0400

    adds madlib and default greeting functions

--- Schoology-lessons/script/index.js
+++ Schoology-lessons/script/index.js
@@ -1,2 +1,25 @@
 //jS
-console.log("Is this thing on?")
+console.log("Is this thing on?");
+
+
+// Write a function that returns Hello + name given
+function hello(name) {
+    console.log(`Hello ${name}`);
+}
+
+const reply = "Mustache!";
+hello(reply);
+
+
+// Write a function that will return "Hello,,,,,You" if no name is given
+function nameDefault(name) {
+    if (name) {
+        console.log(`Hello, ${name}`);
+    } else if (!name) {
+        console.log("Hello,,,,You");
+    }
+}
+
+nameDefault();
+nameDefault(reply);
+
